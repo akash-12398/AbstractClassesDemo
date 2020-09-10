49e2314b85cdb1d6b6fb285a70cc9539db5f1d3e
akash-12398 <napinderbajwa@gmail.com>
2020-09-10 14:06:11 -0500

    first Submission

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ConsoleUI
+{
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle()
+        {
+        }
+        public bool HasSeat { get; set; } = true;
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This Motocycle is in drive");
+        }
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is virtually in drive");
+        }
+    }
+}
---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+
+
+
+        public int Year { get; set; } = 2013;
+        public string Make { get; set; } = "Ford";
+        public string Model { get; set; } = "Escape";
+
+        public abstract void DriveAbstract();// stubbed out method
+
+  
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is virtually in drive");
+        }
+        
+
+
+
+    }
+            
+   
+}
